83365a9f9981f0045b9a1707c541b4553c898f92
Subhrangsu90 <subhrangsubera@outlook.com>
2024-09-10 22:05:49 +0530

    hero section add

--- projects/landing-page/src/app/hero-section/hero-section.component.ts
+++ projects/landing-page/src/app/hero-section/hero-section.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
 export class HeroSectionComponent {
   private router = inject(Router);
-
+  imageBaseUrl: string = environment.imageUrl;
   constructor() {}
   navigateToSignup() {
     this.router.navigate(['/signup']);
---
+++ projects/landing-page/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  // baseURI: window.location.origin + '/',
+  // apiBaseURI: parentbaseurl,
+  //              https://airportapi.centralindia.cloudapp.azure.com
+  imageUrl: 'http://localhost:4201',
+};
